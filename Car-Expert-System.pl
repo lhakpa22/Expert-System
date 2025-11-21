6dee005109e2c582db1f96da57257479a331fe11
rajaad725 <149313727+rajaad725@users.noreply.github.com>
2025-11-21 19:28:49 +0000

    Update Car-Expert-System.pl

--- Car-Expert-System.pl
+++ Car-Expert-System.pl
@@ -1,1 +1,7 @@
-%Car-expert-system.pl
+% Car-expert-system.pl
+% Simple car fault diagnosis expert system (Prolog)
+% Authors: Raja Abdullah Shafique & Lhakpa Tamang
+:- dynamic(known/2).    % known(QuestionAtom, yes/no)
+:- dynamic(asserted_sym/1). % asserted_sym(SymptomAtom)
+:- discontiguous(sympton_question/2).
+:- discontiguous(diagnosis/3).
